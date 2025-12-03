a507126fdf5b343644181ed0e6e5ed21e89b3f27
MonicaFerrerGC <monica.ferrer@alumnos.upm.es>
2025-12-03 15:52:06 +0100

    Add unit tests for TransportationOrderController

--- src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
+++ src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootTest
-@SpringBootApplication
+// @SpringBootTest
 class TransportationOrderServerApplicationTests {
 
 	@Test
